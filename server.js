cfba1fd939a65f010bc1c2210c42d0acc89091b2
Chris Mayol <drlilchris@gmail.com>
2020-06-16 22:19:55 -0400

    updated appname

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/NBAMarket'));
+app.use(express.static('./dist/nbamarket'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname,'/dist/NBAMarket/index.html'));
+res.sendFile(path.join(__dirname,'/dist/nbamarket/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
